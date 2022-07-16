0680ad07603192809e3da99fa5225a4ed6bdd5ad
devaskim <86909129+devaskim@users.noreply.github.com>
2022-07-17 01:55:11 +0500

    Update email.ps1

--- email.ps1
+++ email.ps1
@@ -24,4 +24,4 @@ $port=587
 $password = ConvertTo-SecureString $password -AsPlainText -Force
 $credentials = New-Object System.Management.Automation.PSCredential($email, $password)
 
-Send-MailMessage -SmtpServer $smtp -Port $port -To $email -From $email -Subject $subject2 -Body $body -Encoding UTF8 -UseSsl -Credential $credentials
+Send-MailMessage -SmtpServer $smtp -Port $port -To $email -From $email -Subject $subject -Body $body -Encoding UTF8 -UseSsl -Credential $credentials
