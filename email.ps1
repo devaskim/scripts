8146e26291196691ac1dd1d373bf79b680a7e19b
devaskim <86909129+devaskim@users.noreply.github.com>
2022-07-17 00:01:19 +0500

    Update email.ps1

--- email.ps1
+++ email.ps1
@@ -1,36 +1,31 @@
 ipconfig  | Out-Null 
 [Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("windows-1251")
 
-Write-Host ([System.Text.Encoding]::Default.EncodingName)
-break
+Write-Host "Кодировка: $([System.Text.Encoding]::Default.EncodingName)" 
 
-function ConvertTo-Encoding ([string]$From, [string]$To){
-	Begin{
-		$encFrom = [System.Text.Encoding]::GetEncoding($from)
-		$encTo = [System.Text.Encoding]::GetEncoding($to)
-	}
-	Process{
-		$bytes = $encTo.GetBytes($_)
-		$bytes = [System.Text.Encoding]::Convert($encFrom, $encTo, $bytes)
-		$encTo.GetString($bytes)
-	}
-}
-
-$smtp="smtp.dev.vtb.ru"
-$email="romanovskii@dev.vtb.ru"
-$subject="Тема письма на русском"
-$body="Тестовое письмо"
+$subject="Письмо от Повершелла"
+$body="Большой текст"
 
 Write-Host "---------- BEFORE ENCODING ---------"
 Write-Host $subject
 Write-Host $body
-
-$subject= $subject | ConvertTo-Encoding "windows-1251" "utf-8"
-$body= $body | ConvertTo-Encoding "windows-1251" "utf-8"
-
 Write-Host "---------- AFTER ENCODING ---------"
-Write-Host $subject
-Write-Host $body
 
-$encoding = [System.Text.Encoding]::UTF8
-# Send-MailMessage -SmtpServer $smtp -To $email -From $email -Subject $subject -Body $body -Encoding $encoding
+$password="egwyxnfiwpmnjuqf"
+$email="denisdenisi4@yandex.ru"
+$smtp="smtp.yandex.ru"
+$port=587
+
+$password = ConvertTo-SecureString $password -AsPlainText -Force
+$credentials = New-Object System.Management.Automation.PSCredential($email, $password)
+
+Send-MailMessage `
+-SmtpServer $smtp ` 
+-Port $port ` 
+-To $email `
+-From $email `
+-Subject $subject `
+-Body $body `
+-Encoding UTF8 `
+-UseSsl `
+-Credential $credentials
