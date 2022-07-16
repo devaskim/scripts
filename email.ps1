2a1426fd17c487cd282d0656357b73ff616ab6ba
devaskim <86909129+devaskim@users.noreply.github.com>
2022-07-16 23:13:22 +0500

    Create email.ps1

---
+++ email.ps1
@@ -0,0 +1,33 @@
+ipconfig  | Out-Null 
+[Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("windows-1251")
+
+function ConvertTo-Encoding ([string]$From, [string]$To){
+	Begin{
+		$encFrom = [System.Text.Encoding]::GetEncoding($from)
+		$encTo = [System.Text.Encoding]::GetEncoding($to)
+	}
+	Process{
+		$bytes = $encTo.GetBytes($_)
+		$bytes = [System.Text.Encoding]::Convert($encFrom, $encTo, $bytes)
+		$encTo.GetString($bytes)
+	}
+}
+
+$smtp="smtp.dev.vtb.ru"
+$email="romanovskii@dev.vtb.ru"
+$subject="Тема письма на русском"
+$body="Тестовое письмо"
+
+Write-Host "---------- BEFORE ENCODING ---------"
+Write-Host $subject
+Write-Host $body
+
+$subject= $subject | ConvertTo-Encoding "windows-1251" "utf-8"
+$body= $body | ConvertTo-Encoding "windows-1251" "utf-8"
+
+Write-Host "---------- AFTER ENCODING ---------"
+Write-Host $subject
+Write-Host $body
+
+$encoding = [System.Text.Encoding]::UTF8
+Send-MailMessage -SmtpServer $smtp -To $email -From $email -Subject $subject -Body $body -Encoding $encoding
