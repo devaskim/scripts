6f9b68e3a991de5592306ef70ed243046c73b736
devaskim <86909129+devaskim@users.noreply.github.com>
2022-07-17 00:03:40 +0500

    Update email.ps1

--- email.ps1
+++ email.ps1
@@ -1,5 +1,6 @@
 ipconfig  | Out-Null 
-[Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("windows-1251")
+# [Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("windows-1251")
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 
 Write-Host "Кодировка: $([System.Text.Encoding]::Default.EncodingName)" 
 
