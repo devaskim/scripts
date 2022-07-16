12a7988c65050a394b8f73ebef1c49ed20267a40
devaskim <86909129+devaskim@users.noreply.github.com>
2022-07-17 02:04:23 +0500

    Update email.ps1

--- email.ps1
+++ email.ps1
@@ -9,17 +9,16 @@ $subject="Письмо из Повершелла"
 $body="Тимсити и Повершелл побеждены!!!"
 
 Write-Host "---------- TEXT START ---------"
-Write-Host $subject
-Write-Host $body
+Write-Host "Тема: $subject"
+Write-Host "Письмо: $body"
 Write-Host "---------- TEXT END ---------"
 
-$password="egwyxnfiwpmnjuqf"
 $email="denisdenisi4@yandex.ru"
 $smtp="smtp.yandex.ru"
 $port=587
 
 $password = ConvertTo-SecureString $password -AsPlainText -Force
-$credentials = New-Object System.Management.Automation.PSCredential($email, $password)
+$credentials = New-Object System.Management.Automation.PSCredential($email, "egwyxnfiwpmnjuqf")
 
 Send-MailMessage -SmtpServer $smtp -Port $port -UseSsl -Credential $credentials -To $email -From $email -Subject $(convertToUtf8 $subject) -Body $(convertToUtf8 $body) -Encoding UTF8
 
