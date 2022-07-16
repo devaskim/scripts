9f6920d78b1b03f4d4695b317c11bef017163fe8
devaskim <86909129+devaskim@users.noreply.github.com>
2022-07-17 01:38:47 +0500

    Update email.ps1

--- email.ps1
+++ email.ps1
@@ -10,14 +10,11 @@ $body="Body"
 Write-Host ($subject.Length)
 Write-Host ([System.Text.Encoding]::Default.GetByteCount($subject))
 Write-Host ([System.Text.Encoding]::Default.GetBytes($subject))
-break
 
 Write-Host "---------- TEXT START ---------"
 Write-Host $subject
 Write-Host $body
 Write-Host "---------- TEXT END ---------"
-
-break
 
 $password="egwyxnfiwpmnjuqf"
 $email="denisdenisi4@yandex.ru"
@@ -27,4 +24,4 @@ $port=587
 $password = ConvertTo-SecureString $password -AsPlainText -Force
 $credentials = New-Object System.Management.Automation.PSCredential($email, $password)
 
-Send-MailMessage -SmtpServer $smtp -Port $port -To $email -From $email -Subject $subject -Body $body -Encoding UTF8 -UseSsl -Credential $credentials
+Send-MailMessage -SmtpServer $smtp -Port $port -To $email -From $email -Subject $subject -Body $body -Encoding ([System.Text.Encoding]::Default) -UseSsl -Credential $credentials
