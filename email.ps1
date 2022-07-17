38aba6665dd70149ad1c5c4e722c85a8478e2a94
devaskim <86909129+devaskim@users.noreply.github.com>
2022-07-17 13:36:11 +0500

    Update email.ps1

--- email.ps1
+++ email.ps1
@@ -1,5 +1,4 @@
-# ipconfig  | Out-Null 
-[Console]::OutputEncoding = [System.Text.Encoding]::Default
+ [Console]::OutputEncoding = [System.Text.Encoding]::Default
 
 function convertToUtf8($str) {
     return [System.Text.Encoding]::UTF8.GetString([System.Text.Encoding]::Default.GetBytes($str))
@@ -13,12 +12,11 @@ Write-Host "Тема: $subject"
 Write-Host "Письмо: $body"
 Write-Host "---------- TEXT END ---------"
 
-$password="egwyxnfiwpmnjuqf"
 $email="denisdenisi4@yandex.ru"
 $smtp="smtp.yandex.ru"
 $port=587
 
-$password = ConvertTo-SecureString $password -AsPlainText -Force
+$password = ConvertTo-SecureString "egwyxnfiwpmnjuqf" -AsPlainText -Force
 $credentials = New-Object System.Management.Automation.PSCredential($email, $password)
 
 Send-MailMessage -SmtpServer $smtp -Port $port -UseSsl -Credential $credentials -To $email -From $email -Subject $(convertToUtf8 $subject) -Body $(convertToUtf8 $body) -Encoding UTF8
