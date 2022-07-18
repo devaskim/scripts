1208757b5182bedab64282c4733c03e570db90fd
devaskim <86909129+devaskim@users.noreply.github.com>
2022-07-18 18:11:28 +0500

    Update email_with_attachments.ps1

--- email_with_attachments.ps1
+++ email_with_attachments.ps1
@@ -1,28 +1,24 @@
 [Console]::OutputEncoding = [System.Text.Encoding]::Default
 
-function convertToUtf8($str) {
-    return [System.Text.Encoding]::UTF8.GetString([System.Text.Encoding]::Default.GetBytes($str))
-}
+$email="denisdenisi4@yandex.ru"
+$smtp="smtp.yandex.ru"
 
-$subject="C вложением"
+$subject="Письмо с вложением"
 $body="Тимсити и Повершелл побеждены!!!"
 
 Write-Host "---------- MAIL START ---------"
 Write-Host "Тема письма: $subject"
 Write-Host "Тело письма: $body"
-if ($env:FILES) {
-	Write-Host "Вложения: $env:FILES"
-} else {
-	Write-Host "--нет вложений---"
-}
+Write-Host  $(if ($env:FILES) "Вложения: { $env:FILES" } else { "--нет вложений---" })
 Write-Host "---------- MAIL END ---------"
 
-$CURRENT_DIR=Get-Location
- 
-$from = New-Object System.Net.Mail.MailAddress $email
-$to = New-Object System.Net.Mail.MailAddress $email
+function convertToUtf8($str) {
+    return [System.Text.Encoding]::UTF8.GetString([System.Text.Encoding]::Default.GetBytes($str))
+}
 
-$message = new-object System.Net.Mail.MailMessage $from, $to  
+$CURRENT_DIR=Get-Location
+
+$message = new-object System.Net.Mail.MailMessage($email, $email)
 $message.Subject = $(convertToUtf8 $subject)  
 $message.Body = $(convertToUtf8 $body)
 
@@ -34,19 +30,22 @@ if ($env:FILES) {
 			Write-Host "Файл '$($FILE)' не найден"
 			continue
 		}
-		
+
 		$attachment = new-object System.Net.Mail.Attachment("$CURRENT_DIR\$FILE")
 		$message.Attachments.Add($attachment)
 	}
 }
 
-$server = $smtp
-$client = new-object system.net.mail.smtpclient $server  
+$client = new-object system.net.mail.smtpclient($smtp, $(if ($smtp.IndexOf("vtb") -ne -1) { 25 } else { 587 }))
+if ($client.Port -ne 25) {
+    $client.EnableSsl = $true 
+    $client.Credentials = New-Object System.Net.NetworkCredential($email, "egwyxnfiwpmnjuqf);
+}
  
-"Отправляем письмо адресату {0} через {1} порт {2}." -f $to.ToString(), $client.Host, $client.Port  
+"Отправляем письмо адресату {0} через {1} порт {2}." -f $client.To, $client.Host, $client.Port  
 try {  
    $client.Send($message)  
-   "Письмо от: {1}, кому: {0} успешно отправлено" -f $from, $to  
+   "Письмо от: {1}, кому: {0} успешно отправлено" -f $client.From, $client.To
 } catch {  
   "Ошибка при отправке письма: {0}" -f $Error.ToString()  
 } 
